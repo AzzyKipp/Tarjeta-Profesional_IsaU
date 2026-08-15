0fa39714dced1ed434446f6dab1fd53d05daaac1
Isabella Urrego <isabellaurrego@gmail.com>
2026-08-15 12:14:57 -0500

    Corrección error de tarjeta personal
    se me había olvidado poner la ruta en el .json entonces no lo estaba cogiendo

--- 8thwall/src/app.js
+++ 8thwall/src/app.js
@@ -1,7 +1,7 @@
 const onxrloaded = () => {
   XR8.XrController.configure({
     imageTargetData: [
-      require('../image-targets/objetivo_profe.json'),
+      require('../image-targets/objetivo_profe.json'),('../image-targets/TarjetaAzzy.json'),
     ],
   })
 }
