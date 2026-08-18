fdaf3538cbc635f0d2f45ab74a365d1ee8304084
Isabella Urrego <isabellaurrego@gmail.com>
2026-08-17 19:05:51 -0500

    Redes sociales interactivas

--- 8thwall/src/ExternalURLController.ts
+++ 8thwall/src/ExternalURLController.ts
@@ -3,16 +3,25 @@ import * as ecs from '@8thwall/ecs'
 ecs.registerComponent({
   name: 'ExternalURLController',
 
-  stateMachine: ({world, eid}) => {
-    ecs.defineState('idle')
-      .initial()
-      .onEvent(ecs.input.SCREEN_TOUCH_START, 'touched', {
-        target: eid,
-      })
+  schema: {
+    url: ecs.string,
+  },
 
-    ecs.defineState('touched')
-      .onEnter(() => {
-        console.log('🔥🔥🔥 EL AVATAR RECIBIÓ EL TOUCH')
+  stateMachine: ({world, eid, schemaAttribute}) => {
+    const {url} = schemaAttribute.get(eid)
+
+    ecs.defineState('default')
+      .initial()
+      .listen(world.events.globalId, ecs.input.SCREEN_TOUCH_START, (event) => {
+        console.log('👆 TOUCH')
+        console.log('🎯 TARGET:', event.target)
+
+        if (event.target === eid) {
+          console.log('🔥 MODELO TOCADO:', eid)
+          console.log('🔗 URL:', url)
+
+          window.open(url, '_blank', 'noopener,noreferrer')
+        }
       })
   },
 })
